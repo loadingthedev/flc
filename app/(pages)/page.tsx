d60eb2fbe0bc6c6a08802e609e9ea9c822b84b3c
loadingthedev <loadingthedev@gmail.com>
2025-07-08 15:52:05 +0530

    chore: remove package-lock.json and update layout and home page metadata for FLCLegal

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -4,6 +4,7 @@ import RecentReviewsCarousel from "@/components/Cient";
 import ContactUs from "@/components/Contact";
 import FlcHero from "@/components/Hero";
 import How from "@/components/How-to";
+import Script from "next/script";
 
 export default function Home() {
   return (
@@ -15,6 +16,25 @@ export default function Home() {
       <How />
       <RecentReviewsCarousel />
       <ContactUs />
+
+      {/* <!-- Add this to your website's HTML body --> */}
+      <Script id="ll-chat-widget-config" strategy="afterInteractive">
+        {`
+  window.__CHAT_WIDGET_CONFIG__ = {
+    token: "lil-c7dfc4f1-aa6b-44b3-986a-0b93f186c22e",
+    botName: "FLCLegal",
+    primaryColor: "#093145",
+    position: "bottom-right", // or "bottom-right"
+    url: "https://linkinlegal-chat.vercel.app",
+  };
+  `}
+      </Script>
+      <Script
+        id="ll-chat-widget-loader"
+        src="https://linkinlegal-chat.vercel.app/widget-loader.js"
+        defer
+        strategy="afterInteractive"
+      ></Script>
     </>
   );
 }
